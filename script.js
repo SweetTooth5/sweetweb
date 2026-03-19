10a4c8fce7b132f054c4cd7892a12c5f9fa2f085
SweetTooth5 <artwelgido@gmail.com>
2026-03-19 07:24:31 +0200

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-// Function to toggle the message box visibility
-function toggleMessageBox() {
-    const form = document.getElementById('messageForm');
-    if (form.style.display === 'block') {
-        form.style.display = 'none';
-    } else {
-        form.style.display = 'block';
-    }
-}
